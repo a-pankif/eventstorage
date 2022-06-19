b1c8a587f1c6f757d3226deed91bb46038e13ff0
Alexey Pankiv <a@pankif.com>
2022-06-19 12:37:46 +0300

    refactr eventsfiles_test.go

--- eventsfiles_test.go
+++ eventsfiles_test.go
@@ -2,193 +2,190 @@ package eventstorage
 
 import (
 	"bytes"
-	"fmt"
 	"strings"
 	"testing"
 )
 
 func Test_eventStorage_initRegistryFile(t *testing.T) {
-	b := &eventStorage{
+	s := &eventStorage{
 		basePath: t.TempDir(),
 		write:    &write{buf: new(bytes.Buffer), fileMaxSize: 100 * MB},
 		read:     &read{readableFiles: make(readableFiles), buf: new(strings.Builder)},
 	}
 
-	t.Cleanup(b.Shutdown)
+	t.Cleanup(s.Shutdown)
 
-	if err := b.initFilesRegistry(); err != nil {
+	if err := s.initFilesRegistry(); err != nil {
 		t.Errorf("filesRegistry not inited")
 		return
 	}
 }
 
 func Test_eventStorage_initRegistryFileFailed(t *testing.T) {
-	b := &eventStorage{
+	s := &eventStorage{
 		basePath: string([]byte{0}),
 		write:    &write{buf: new(bytes.Buffer), fileMaxSize: 100 * MB},
 		read:     &read{readableFiles: make(readableFiles), buf: new(strings.Builder)},
 	}
-	t.Cleanup(b.Shutdown)
+	t.Cleanup(s.Shutdown)
 
-	if err := b.initFilesRegistry(); err == nil {
+	if err := s.initFilesRegistry(); err == nil {
 		t.Errorf("filesRegistry inited, but expected fail")
 	}
 }
 
 func Test_eventStorage_appendInRegistryFile(t *testing.T) {
-	b := &eventStorage{
+	s := &eventStorage{
 		basePath: t.TempDir(),
 		write:    &write{buf: new(bytes.Buffer), fileMaxSize: 100 * MB},
 		read:     &read{readableFiles: make(readableFiles), buf: new(strings.Builder)},
 	}
-	t.Cleanup(b.Shutdown)
-	_ = b.initFilesRegistry()
+	t.Cleanup(s.Shutdown)
+	_ = s.initFilesRegistry()
 
-	file1, _ := b.openEventsFile(1, true)
-	file2, _ := b.openEventsFile(2, true)
-	file3, _ := b.openEventsFile(3, true)
+	file1, _ := s.openEventsFile(1, true)
+	file2, _ := s.openEventsFile(2, true)
+	file3, _ := s.openEventsFile(3, true)
 
 	_ = file1.Close()
 	_ = file2.Close()
 	_ = file3.Close()
-	_ = b.filesRegistry.Close()
+	_ = s.filesRegistry.Close()
 
-	if len(b.read.readableFiles) != 3 {
+	if len(s.read.readableFiles) != 3 {
 		t.Errorf("appendInRegistryFile has wrong count")
 		return
 	}
 
-	for i, file := range b.read.readableFiles {
+	for i, file := range s.read.readableFiles {
 		info, _ := file.Stat()
 
-		if b.getFileName(i) != info.Name() {
-			t.Errorf("Wrong file name, expected %v, got %v", b.getFileName(i), info.Name())
+		if s.getFileName(i) != info.Name() {
+			t.Errorf("Wrong file name, expected %v, got %v", s.getFileName(i), info.Name())
 			return
 		}
 	}
 }
 
 func Test_eventStorage_initLogFileWithoutRegistry(t *testing.T) {
-	b := &eventStorage{
+	s := &eventStorage{
 		write: &write{buf: new(bytes.Buffer), fileMaxSize: 100 * MB},
 		read:  &read{readableFiles: make(readableFiles), buf: new(strings.Builder)},
 	}
 
-	if err := b.initEventsFile(); err == nil {
+	if err := s.initEventsFile(); err == nil {
 		t.Errorf("initEventsFile expected failed without registry")
 	}
 
-	t.Cleanup(b.Shutdown)
+	t.Cleanup(s.Shutdown)
 }
 
 func Test_eventStorage_initLogFile(t *testing.T) {
-	b := &eventStorage{
+	s := &eventStorage{
 		basePath: t.TempDir(),
 		write:    &write{buf: new(bytes.Buffer), fileMaxSize: 100 * MB},
 		read:     &read{readableFiles: make(readableFiles), buf: new(strings.Builder)},
 	}
-	_ = b.initFilesRegistry()
+	_ = s.initFilesRegistry()
 
-	if err := b.initEventsFile(); err != nil {
+	if err := s.initEventsFile(); err != nil {
 		t.Errorf("initEventsFile failed")
 		return
 	}
 
-	t.Cleanup(b.Shutdown)
+	t.Cleanup(s.Shutdown)
 }
 
 func Test_eventStorage_initLogFileFailed(t *testing.T) {
-	b := &eventStorage{
+	s := &eventStorage{
 		basePath: t.TempDir(),
 		write:    &write{buf: new(bytes.Buffer), fileMaxSize: 100 * MB},
 		read:     &read{readableFiles: make(readableFiles), buf: new(strings.Builder)},
 	}
 
-	_ = b.initFilesRegistry()
+	_ = s.initFilesRegistry()
 
-	t.Cleanup(b.Shutdown)
+	t.Cleanup(s.Shutdown)
 
-	b.basePath = string([]byte{0})
+	s.basePath = string([]byte{0})
 
-	if err := b.initEventsFile(); err == nil {
+	if err := s.initEventsFile(); err == nil {
 		t.Errorf("initEventsFile expect failed")
 	}
 }
 
 func Test_eventStorage_rotateLogFileFailedCloseOld(t *testing.T) {
-	b := &eventStorage{
+	s := &eventStorage{
 		write: &write{buf: new(bytes.Buffer), fileMaxSize: 100 * MB},
 		read:  &read{readableFiles: make(readableFiles), buf: new(strings.Builder)},
 	}
 
-	if err := b.rotateEventsFile(); err == nil {
+	if err := s.rotateEventsFile(); err == nil {
 		t.Errorf("rotateLogFileFailedCloseOld expect failed")
 	}
 
-	t.Cleanup(b.Shutdown)
+	t.Cleanup(s.Shutdown)
 }
 
 func Test_eventStorage_rotateLogFile(t *testing.T) {
-	b := &eventStorage{
+	s := &eventStorage{
 		basePath: t.TempDir(),
 		write:    &write{buf: new(bytes.Buffer), fileMaxSize: 100 * MB},
 		read:     &read{readableFiles: make(readableFiles), buf: new(strings.Builder)},
 	}
 
-	_ = b.initFilesRegistry()
-	_ = b.initEventsFile()
+	_ = s.initFilesRegistry()
+	_ = s.initEventsFile()
 
-	t.Cleanup(b.Shutdown)
+	t.Cleanup(s.Shutdown)
 
-	if err := b.rotateEventsFile(); err != nil {
+	if err := s.rotateEventsFile(); err != nil {
 		t.Errorf("rotateEventsFile failed")
 		return
 	}
 
-	if len(b.read.readableFiles) != 2 {
+	if len(s.read.readableFiles) != 2 {
 		t.Errorf("rotateLogFile has wrong count")
 		return
 	}
 
-	for i, file := range b.read.readableFiles {
+	for i, file := range s.read.readableFiles {
 		info, _ := file.Stat()
 
-		if b.getFileName(i) != info.Name() {
-			t.Errorf("Wrong file name, expected %v, got %v", b.getFileName(i), info.Name())
+		if s.getFileName(i) != info.Name() {
+			t.Errorf("Wrong file name, expected %v, got %v", s.getFileName(i), info.Name())
 			return
 		}
 	}
 }
 
 func Test_eventStorage_openLogFileFailedAppend(t *testing.T) {
-	b := &eventStorage{
+	s := &eventStorage{
 		write: &write{buf: new(bytes.Buffer), fileMaxSize: 100 * MB},
 		read:  &read{readableFiles: make(readableFiles), buf: new(strings.Builder)},
 	}
-	t.Cleanup(b.Shutdown)
+	t.Cleanup(s.Shutdown)
 
-	if _, err := b.openEventsFile(1, true); err == nil {
+	if _, err := s.openEventsFile(1, true); err == nil {
 		t.Errorf("openLogFileFailedAppend expect failed")
 	}
 }
 
 func Test_eventStorage_SetLogFileSize(t *testing.T) {
-	b := &eventStorage{
+	s := &eventStorage{
 		write: &write{buf: new(bytes.Buffer), fileMaxSize: 100 * MB},
 		read:  &read{readableFiles: make(readableFiles), buf: new(strings.Builder)},
 	}
-	b.SetWriteFileMaxSize(100)
-	t.Cleanup(b.Shutdown)
+	s.SetWriteFileMaxSize(100)
+	t.Cleanup(s.Shutdown)
 
-	if b.write.fileMaxSize != 100 {
+	if s.write.fileMaxSize != 100 {
 		t.Errorf("SetWriteFileMaxSize failed")
 	}
 }
 
 func Test_eventStorage_calculateLogFileSize(t *testing.T) {
-	dir := t.TempDir()
-	fmt.Println(dir)
-	s, err := New(dir)
+	s, err := New(t.TempDir())
 
 	if err != nil {
 		t.Errorf(err.Error())
@@ -200,7 +197,7 @@ func Test_eventStorage_calculateLogFileSize(t *testing.T) {
 
 	_, _ = s.Write([]byte{1, 2, 3})
 
-	// if s.calculateWriteFileSize() != 4 { // 3 is data length + 1 is line break
-	// 	t.Errorf("calculateWriteFileSize failed")
-	// }
+	if s.calculateWriteFileSize() != 4 { // 3 is data length + 1 is line break
+		t.Errorf("calculateWriteFileSize failed")
+	}
 }
